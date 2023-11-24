bf6a415eed25d43b8999596a54d9adce9abd4ecb
Mrmatrixx77 <kunalsk2001@gmail.com>
2023-11-24 21:38:52 +0530

    utils and errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 // require("dotenv").config({path: "./env"}); 
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-// import { DB_NAME } from './constants.js';
-// import { connectDB } from "./db/db.js";
 import express from "express";
 dotenv.config({
     path: "./env"
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+//PROMISES SYNTAX
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+//try catch and async await sytnax
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
